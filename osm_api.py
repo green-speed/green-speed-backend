a1e1b981a9399c2870dd02ecefe6a361592377f3
hlib <hlibbabii@gmail.com>
2019-11-15 23:46:24 +0200

    getting coordinates by geoobject name

---
+++ osm_api.py
@@ -0,0 +1,13 @@
+import requests
+from typing import Tuple
+
+
+def get_coordinates_by_name(name: str) -> Tuple[str, str]:
+    params = {'q': name, 'format': 'json'}
+    a = requests.get("https://nominatim.openstreetmap.org/search", params=params)
+    res = a.json()
+    return res[0]['lon'], res[0]['lat']
+
+
+coords = get_coordinates_by_name('lago di caldaro')
+print(coords)
