2d350e2548bcc18f599e0441737c910639a5be97
hlib <hlibbabii@gmail.com>
2019-11-16 01:30:05 +0200

    returning fake json

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+import os
+
+from flask import Flask, jsonify, abort
 from flask import request
 
 app = Flask(__name__)
+
+current_script_location = os.path.realpath(__file__)
+root_package_dir = os.path.dirname(current_script_location)
 
 
 @app.route('/route')
@@ -15,6 +20,18 @@ def find_bus():
     print('to_x: ', to_x)
     print('to_y: ', to_y)
     return '''<h1>from_x: {}</h1>'''.format(from_x)
+
+
+@app.route('/api/v1/directions')
+def get_directions():
+    from_location = request.args.get('from') #if key doesn't exist, returns None
+    to_location = request.args.get('to')
+    if from_location or to_location:
+        with open(os.path.join(root_package_dir, 'sample_transportaion_options.json'), 'r') as f:
+            s = f.read()
+        return jsonify(s)
+    else:
+        abort(404)
 
 
 if __name__ == '__main__':
